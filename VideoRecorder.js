1d89c24aa3aa151cbdac8b29a0781fd516abdd3e
CatPerry <27353719+CatPerry@users.noreply.github.com>
2018-12-13 17:08:32 -0500

    Adds a few new icons to replace current ones,a nd adjusts default camera settings

--- VideoRecorder.js
+++ VideoRecorder.js
@@ -1,10 +1,54 @@
 import React from 'react';
 import { Button, Text, Header, Body, Icon, Title, Spinner } from 'native-base';
 import { Camera, Permissions, FileSystem } from 'expo';
-import { StyleSheet, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Slider, Platform } from 'react-native';
 import Layout from './Layout';
 import delay from 'delay';
 import shortid from 'shortid';
+import {
+  Ionicons,
+  MaterialIcons,
+  Foundation,
+  MaterialCommunityIcons,
+  Octicons
+} from '@expo/vector-icons';
+
+
+const endpoint = 'http://localhost:3000';
+
+const flashModeOrder = {
+  off: 'on',
+  on: 'auto',
+  auto: 'torch',
+  torch: 'off',
+};
+
+const flashIcons = {
+  off: 'flash-off',
+  on: 'flash-on',
+  auto: 'flash-auto',
+  torch: 'highlight'
+};
+
+const wbOrder = {
+  auto: 'sunny',
+  sunny: 'cloudy',
+  cloudy: 'shadow',
+  shadow: 'fluorescent',
+  fluorescent: 'incandescent',
+  incandescent: 'auto',
+};
+
+const wbIcons = {
+  auto: 'wb-auto',
+  sunny: 'wb-sunny',
+  cloudy: 'wb-cloudy',
+  shadow: 'beach-access',
+  fluorescent: 'wb-iridescent',
+  incandescent: 'wb-incandescent',
+};
+
+
 
 class RedirectTo extends React.Component {
   componentDidMount() {
@@ -39,7 +83,23 @@ export default class VideoRecorder extends React.Component {
     type: Camera.Constants.Type.front,
     recording: false,
     duration: 0,
-    redirect: false
+    redirect: false,
+    flash: 'off',
+    zoom: 0,
+    autoFocus: 'on',
+    whiteBalance: 'auto',
+    ratio: '16:9',
+    ratios: [],
+    barcodeScanning: false,
+    faceDetecting: false,
+    faces: [],
+    newPhotos: false,
+    permissionsGranted: false,
+    pictureSize: undefined,
+    pictureSizes: [],
+    pictureSizeId: 0,
+    showGallery: false,
+    showMoreOptions: false,
   };
 
   async componentWillMount() {
@@ -61,6 +121,14 @@ export default class VideoRecorder extends React.Component {
     }
   }
 
+  getRatios = async () => {
+    const ratios = await this.camera.getSupportedRatios();
+    return ratios;
+  };
+
+  toggleFlash = () => this.setState({ flash: flashModeOrder[this.state.flash] });
+
+
   async startRecording() {
     if (!this.camera) {
       return;
@@ -68,7 +136,7 @@ export default class VideoRecorder extends React.Component {
 
     await this.setState(state => ({ ...state, recording: true }));
     this.registerRecord();
-    const record = await this.camera.recordAsync();
+    const { uri, codec = 'mp4', record } = await this.camera.recordAsync();
     console.log(record);
     const videoId = shortid.generate();
 
@@ -78,10 +146,10 @@ export default class VideoRecorder extends React.Component {
 
     await FileSystem.moveAsync({
       from: record.uri,
-      to: `${FileSystem.documentDirectory}videos/demo_${videoId}.mov`
+      to: `${FileSystem.documentDirectory}videos/demo_${videoId}.${codec}`
     });
 
-    console.log(`${FileSystem.documentDirectory}videos/demo_${videoId}.mov`);
+    console.log(`${FileSystem.documentDirectory}videos/demo_${videoId}.${codec}`);
     this.setState(state => ({ ...state, redirect: 'MyVideos' }));
   }
 
@@ -99,6 +167,53 @@ export default class VideoRecorder extends React.Component {
 
     return recording ? this.stopRecording() : this.startRecording();
   }
+
+  // renderGallery() {
+  //   return <GalleryScreen onPress={this.toggleView.bind(this)} />;
+  // }
+
+  renderTopBar = () =>
+    <View
+      style={styles.topBar}>
+      <TouchableOpacity style={styles.toggleButton} onPress={this.toggleFacing}>
+        <Ionicons name="ios-reverse-camera" size={32} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.toggleButton} onPress={this.toggleFlash}>
+        <MaterialIcons name={flashIcons[this.state.flash]} size={32} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.toggleButton} onPress={this.toggleWB}>
+        <MaterialIcons name={wbIcons[this.state.whiteBalance]} size={32} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.toggleButton} onPress={this.toggleFocus}>
+        <Text style={[styles.autoFocusLabel, { color: this.state.autoFocus === 'on' ? "white" : "#6b6b6b" }]}>AF</Text>
+      </TouchableOpacity>
+    </View>
+
+  renderBottomBar = () =>
+    <View
+      style={styles.bottomBar}>
+      <TouchableOpacity style={styles.bottomButton} onPress={this.toggleMoreOptions}>
+        <Octicons name="kebab-horizontal" size={30} color="white" />
+      </TouchableOpacity>
+      <View style={{ flex: 0.4 }}>
+        <TouchableOpacity
+          onPress={this.takePicture}
+          style={{ alignSelf: 'center' }}
+        >
+          <Ionicons name="ios-radio-button-on" size={70} color="white" />
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity style={styles.bottomButton} onPress={this.toggleView}>
+        <View>
+          <Foundation name="thumbnails" size={30} color="white" />
+          {this.state.newPhotos && <View style={styles.newPhotosDot} />}
+        </View>
+      </TouchableOpacity>
+    </View>
+
+
+
+
 
   render() {
     const { hasCameraPermission, recording, duration, redirect } = this.state;
@@ -128,6 +243,7 @@ export default class VideoRecorder extends React.Component {
             ref={ref => {
               this.camera = ref;
             }}
+            flashMode={this.state.flash}
           >
             <View style={styles.topActions}>
               {recording && (
@@ -155,7 +271,7 @@ export default class VideoRecorder extends React.Component {
                 <Icon ios="ios-reverse-camera" android="md-reverse-camera" />
               </Button>
             </View>
-            <View style={styles.bottonActions}>
+            <View style={styles.buttonActions}>
               <Button transparent onPress={() => this.setState({ redirect: 'Home' })}>
                 <Icon ios="ios-home" android="md-home" />
               </Button>
@@ -175,6 +291,8 @@ export default class VideoRecorder extends React.Component {
                 <Icon ios="ios-folder" android="md-folder" />
               </Button>
             </View>
+            {this.renderTopBar()}
+            {this.renderBottomBar()}
           </Camera>
         </Layout>
       );
@@ -193,7 +311,7 @@ const styles = StyleSheet.create({
   chronometer: {
     margin: 10
   },
-  bottonActions: {
+  buttonActions: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-around',
@@ -208,5 +326,20 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'space-between'
+  },
+  topBar: {
+    flex: 0.2,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    paddingTop: 5,
+  },
+  bottomBar: {
+    paddingBottom: 5,
+    backgroundColor: 'transparent',
+    alignSelf: 'flex-end',
+    justifyContent: 'space-between',
+    flex: 0.12,
+    flexDirection: 'row',
   }
 });
